5edd0d5a86da9b7d33668699d86a470d333485e5
Dominic-Zucco <dominic-zucco@uiowa.edu>
2015-02-27 08:53:47 -0600

    Updated the Unit tests

--- UI_Scheduler_Tool.Tests/WrapperTests/MauiWrapperTest.cs
+++ UI_Scheduler_Tool.Tests/WrapperTests/MauiWrapperTest.cs
@@ -12,10 +12,40 @@ namespace UI_Scheduler_Tool.Tests.WrapperTests
         {
             string result;
             result = MauiWrapper.GetCourse("055:032");
-            result = MauiWrapper.GetMinors();
-            result = MauiWrapper.GetProgramsOfStudyByNatKey("R");
-            result = MauiWrapper.GetProgramOfStudyByProgramNatKey("ANTH");
-            result = MauiWrapper.GetProgramOfStudyByID("305");
+            //result = MauiWrapper.GetMinors();
+            Boolean contains_value = result.Contains("Digital Design");
+            Assert.IsTrue(contains_value);
+            //result = MauiWrapper.GetProgramsOfStudyByNatKey("R");
+            //result = MauiWrapper.GetProgramOfStudyByProgramNatKey("ANTH");
+            //result = MauiWrapper.GetProgramOfStudyByID("305");
         }
+
+        [TestMethod]
+        public void getMinors()
+        {
+            string result;
+            result = MauiWrapper.GetMinors();
+            Boolean contains_value = result.Contains("Aerospace Studies");
+            Assert.IsTrue(contains_value);
+        }
+
+        [TestMethod]
+        public void getProgramsofStudyByNatKey()
+        {
+            string result;
+            result = MauiWrapper.GetProgramsOfStudyByNatKey("R");
+            Boolean contains_value = result.Contains("Aerospace Studies");
+            Assert.IsTrue(contains_value);
+        }
+
+        [TestMethod]
+        public void getProgramofStudyByID()
+        {
+            string result;
+            result = MauiWrapper.GetProgramOfStudyByID("305");
+            Boolean contains_value = result.Contains("Communication Studies");
+            Assert.IsTrue(contains_value);
+        }
+
     }
 }
