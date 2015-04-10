def922bc84792ac023251c06534ed3a0e2cc11fa
egunnink <ezekiel-gunnink@uiowa.edu>
2015-04-10 11:20:45 -0500

    added base views and controller for track

---
+++ UI Scheduler Tool/Controllers/TrackController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI_Scheduler_Tool.Controllers
+{
+    public class TrackController : Controller
+    {
+        public ActionResult Select()
+        {
+            return View();
+        }
+
+        public ActionResult EFA()
+        {
+            return View();
+        }
+
+        public ActionResult TrackBuilder()
+        {
+            return View();
+        }
+    }
+}
