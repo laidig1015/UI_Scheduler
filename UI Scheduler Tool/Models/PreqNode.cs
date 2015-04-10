e9dd1043b7c1ed734e8e3ef10e7ffde3d1d31c40
egunnink <ezekiel-gunnink@uiowa.edu>
2015-04-10 18:20:45 -0500

    more js work

--- UI Scheduler Tool/Models/PreqNode.cs
+++ UI Scheduler Tool/Models/PreqNode.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace UI_Scheduler_Tool.Models
+namespace UI_Scheduler_Tool.Models.Json
 {
+    // NOTE: this is supposed to be serialied to json so we us the pascalCase convension
+    // that is more common in js 
     public class PreqNode
     {
-        public Course CourseInfo { get; set;}
-        public List<PreqNode> Children { get; set; }
-        public List<PreqNode> Parents { get; set; }
+        public string name { get; set; }
+        public string id { get; set; }
+        public string hours { get; set; }
+        public bool isOfferedInFall { get; set; }
+        public bool isOfferedInSpring { get; set; }
+        public string description { get; set; }
+        public string[] parents { get; set; }
+        public string[] children { get; set; }
 
         public PreqNode(Course c)
         {
-            CourseInfo = c;
-            Children = new List<PreqNode>();
-            Parents = new List<PreqNode>();
+            name = c.CourseName;
+            id = c.CourseNumber;
+            hours = c.CreditHours;
+            isOfferedInFall = true;// TODO
+            isOfferedInSpring = true;
+            parents = c.Parents.Select(p => p.Parent.CourseNumber).ToArray();
+            children = c.Children.Select(p => p.Child.CourseNumber).ToArray();
         }
     }
 }
