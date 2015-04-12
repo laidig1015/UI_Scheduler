0dbb0f7be2bc678738e43e1862af5aad6ebda2f4
egunnink <ezekiel-gunnink@uiowa.edu>
2015-04-12 17:14:34 -0500

    initial draft of preq checks added

--- UI Scheduler Tool/DataMigrations/Configuration.cs
+++ UI Scheduler Tool/DataMigrations/Configuration.cs
@@ -18,35 +18,66 @@ namespace UI_Scheduler_Tool.DataMigrations
         {
             //  This method will be called after migrating to the latest version.
 
+            // basic tree test nodes
             // root courses
-            Course rootReq = new Course { CourseName = "RootRequired", CourseNumber = "ROOT:0000", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
-            Course rootOptA = new Course { CourseName = "RootOptionalA", CourseNumber = "ROOT:1000", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
-            Course rootOptB = new Course { CourseName = "RootOptionalB", CourseNumber = "ROOT:1001", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course rootReq = new Course { CourseName = "RootRequired", CourseNumber = "ROOT:0000", CatalogDescription = "TREE_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course rootOptA = new Course { CourseName = "RootOptionalA", CourseNumber = "ROOT:1000", CatalogDescription = "TREE_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course rootOptB = new Course { CourseName = "RootOptionalB", CourseNumber = "ROOT:1001", CatalogDescription = "TREE_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
 
             // easy preq
-            Course easy = new Course { CourseName = "Easy", CourseNumber = "EZ:0000", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course easy = new Course { CourseName = "Easy", CourseNumber = "EZ:0000", CatalogDescription = "TREE_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
 
             // advanced preq
-            Course advancedA = new Course { CourseName = "AdvancedA", CourseNumber = "ADV:0000", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
-            Course advancedB = new Course { CourseName = "AdvancedB", CourseNumber = "ADV:0001", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course advancedA = new Course { CourseName = "AdvancedA", CourseNumber = "ADV:0000", CatalogDescription = "TREE_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course advancedB = new Course { CourseName = "AdvancedB", CourseNumber = "ADV:0001", CatalogDescription = "TREE_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+
+            // basic preq check nodes
+            // level 0
+            Course N0A = new Course { CourseName = "N0A", CourseNumber = "N0:0001", CatalogDescription = "PREQ_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course N0B = new Course { CourseName = "N0B", CourseNumber = "N0:0002", CatalogDescription = "PREQ_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course N0C = new Course { CourseName = "N0C", CourseNumber = "N0:0003", CatalogDescription = "PREQ_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            // level 1
+            Course N1A = new Course { CourseName = "N1A", CourseNumber = "N1:0001", CatalogDescription = "PREQ_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course N1B = new Course { CourseName = "N1B", CourseNumber = "N1:0002", CatalogDescription = "PREQ_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            Course N1C = new Course { CourseName = "N1C", CourseNumber = "N1:0003", CatalogDescription = "PREQ_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
+            // level 2
+            Course N2A = new Course { CourseName = "N2A", CourseNumber = "N2:0001", CatalogDescription = "PREQ_TEST", CreditHours = "0", Occurence = 0, LastTaughtID = 0 };
 
             context.Courses.AddOrUpdate(
+                // tree test
                 rootReq, rootOptA, rootOptB,
                 easy,
-                advancedA, advancedB
+                advancedA, advancedB,
+                // preq checks
+                N0A, N0B, N0C,
+                N1A, N1B, N1C,
+                N2A
                 );
+
+            // basic preq check nodes
 
             context.SaveChanges();
 
             context.PreqEdges.AddOrUpdate(
-                // add level 1 edges
+                // tree test
+                // add level 0 edges
                 new PreqEdge { Parent = rootReq, Child = easy, IsRequired = true },
                 new PreqEdge { Parent = rootOptA, Child = easy, IsRequired = false },
                 new PreqEdge { Parent = rootOptB, Child = easy, IsRequired = false },
 
-                // add level 2 edges
+                // add level 1 edges
                 new PreqEdge { Parent = easy, Child = advancedA, IsRequired = true },
-                new PreqEdge { Parent = easy, Child = advancedB, IsRequired = true }
+                new PreqEdge { Parent = easy, Child = advancedB, IsRequired = true },
+
+                // preq check
+                // add level 0 edges
+                new PreqEdge { Parent = N0A, Child = N1A, IsRequired = true },
+                new PreqEdge { Parent = N0B, Child = N1A, IsRequired = true },
+                new PreqEdge { Parent = N0C, Child = N1B, IsRequired = true },
+                // add level 1 edges
+                new PreqEdge { Parent = N1A, Child = N2A, IsRequired = true },
+                new PreqEdge { Parent = N1B, Child = N2A, IsRequired = false },
+                new PreqEdge { Parent = N1C, Child = N2A, IsRequired = true }
                 );
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
--- UI Scheduler Tool/Scripts/Preq.js
+++ UI Scheduler Tool/Scripts/Preq.js
@@ -8,7 +8,7 @@ function TrackMatrix() {
     for (var i = 0; i < MAX_SEMSTERS; i++) {
         this.matrix[i] = new Array(MAX_CLASSES);
     }
-    this.preq = {};
+    this.nodes = {};
     this.totalSemsterHours = 0;
 }
 
@@ -30,50 +30,102 @@ TrackMatrix.prototype.addNodes = function (nodes) {
     var nodesLen = nodes.length;
     for (var i = 0; i < nodesLen; i++) {
         var node = nodes[i];
-        if (!(node.id in this.preq)) {
-            this.preq[node.id] = node;
+        if (!(node.id in this.nodes)) {
+            this.nodes[node.id] = node;
         }
     }
-    console.log("updated preq: %o", this.preq);
+    console.log("updated preq: %o", this.nodes);
 }
 
-TrackMatrix.prototype.addCourse = function (semsterId, nodeId) {
+TrackMatrix.prototype.addCourse = function (semester, nodeId) {
+    // first make sure we haven't taken this class already
+    var takenIn = this.hasTaken(nodeId);
+    if (takenIn != -1) {
+        console.log("class already taken in semster %d", takenIn + 1);// offset from 0 index
+        return { wasSuccessful: false, errorType: "TAKEN", takenIn: takenIn };
+    }
+
     // first find our course node in our hash
     // if we don't have it try to update from our database
-    if (!(nodeId in this.preq) && !this.loadNodes(nodeId)) {
+    if (!this.resolveNode(nodeId)) {
         console.log("failed to load %s from db", nodeId);
-        return false;
+        return { wasSuccessful: false, errorType: "LOAD" };
     }
-    var node = this.preq[nodeId];
+
+    var node = this.nodes[nodeId];
 
     // first check if the course is offered in this semester
-    if (TrackMatrix.nodeIsOfferedIn(semsterId, node)) {
-        console.log("%s is not available in %s", nodeId, (semsterId & 1 ? "spring" : "fall"));
-        return false;
+    if (TrackMatrix.nodeIsOfferedIn(semester, node)) {
+        console.log("%s is not available in %s", nodeId, (semester & 1 ? "spring" : "fall"));
+        return { wasSuccessful: false, errorType: "OFFERING" };
     }
 
-    // go through all parents of our nodes 
-    var curr = node;
-    var childI = 0;
-    var semester = semsterId;
-    while (childI > 0) {
-        if (this.nodeIsInSemster(semester, curr)) {
+    // check if we have the required prequisties to take the course
+    var dirty = this.checkPreq(semester, node);
+    if (dirty.length > 0) {
+        return { wasSuccessful: false, errorType: "PREQ", dirty: dirty };
+    }
 
+}
+
+TrackMatrix.prototype.resolveNode = function (nodeId) {
+    // check if we don't have it in our hash table
+    // if we don't then try to load it from our db
+    return (!(nodeId in this.nodes) && !this.loadNodes(nodeId));
+}
+
+TrackMatrix.prototype.hasTaken = function (node) {
+    return hasTakenSince(MAX_SEMSTERS - 1, node);
+}
+
+TrackMatrix.prototype.hasTakenSince = function(semester, nodeId) {
+    for (var s = semester; s >= 0; s--) {
+        if (this.matrix[s].indexOf(nodeId) != -1) {
+            return s;
         }
     }
+    return -1;
+}
+
+TrackMatrix.prototype.checkPreq = function (semester, node) {
+    // go through all parents of our nodes
+    var dirty = new Array();
+    var stack = new Array();
+    var numInitialParents = node.parents.length;
+    // start by pushing our initial nodes parents to the stack
+    for (var i = 0; i < numInitialParents; i++) {
+        stack.push({ node: this.nodes[node.parents[i]], index: semsterId });
+    }
+
+    while (stack.length > 0) {// while we have no more nodes to check
+        var currFrame = stack.pop();// get the current stack frame we are looking at
+
+        // first check if we have taken this class from the semester we are stupposed to
+        // start checking it from
+        var takenAt = this.hasTakenSince(currFrame.index, currFrame.node.id);
+        if (takenAt == -1) {
+            // if we didnt' take it then mark it as dirty
+            dirty.push(currFrame);
+        } else {
+            // if we did take it then add its parents to the stack to be checked
+            // since we did find it all of the parents should start their search
+            // from the index we found the class at
+            var numParents = curr.node.parents.length;
+            for (var i = 0; i < numParents; i++) {
+                stack.push({ node: curr.node, index: takenAt });
+            }
+        }
+    }
+    return dirty;// return an array of our dirty nodes
 }
 
 TrackMatrix.nodeIsOfferedIn = function (semsterId, node) {
     var isSpring = semsterId & 1;// is odd and we assume we start in fall
     return ((isSpring && node.isOfferedInSpring) || (!isSpring && node.isOfferedInFall));
-};
-
-TrackMatrix.prototype.nodeIsInSemster = function (semester, node) {
-    return node.id in this.matrix[semester];
-};
+}
 
 TrackMatrix.prototype.removeCourse = function (semster, courseID) {
 
-};
+}
 
 
